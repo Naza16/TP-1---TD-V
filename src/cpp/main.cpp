7c757a6c291812eaea10e5a64c87e6b6f176125e
Lukey <lucianopozzoli@icloud.com>
2024-04-09 14:57:37 -0300

    Lista, Optimalidad, Factibilidad C++
    Las 3 funciones estan escritas en c++ pero ninguna esta testeada todavia.

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -3,8 +3,49 @@
 #include <fstream>
 #include "include/json.hpp"
 
+
 // Para libreria de JSON.
 using namespace nlohmann;
+
+//Lista NO TESTEADA
+std::vector<std::pair<double,double>> lista_breakpoints(int m, int n, std::vector<double> grid_x, std::vector<double> grid_y){
+    int i = 0;
+    std::vector<std::pair<double,double>> resultado;
+    while(i < m){
+        resultado[i].first = grid_x[i];
+        resultado[i].second = grid_y[i];
+        }
+    return resultado;
+}
+//Lista NO TESTEADA
+double optimalidad(std::vector<std::pair<double,double>> secuencia, std::vector<double> x, std::vector<double> y) {
+    double error = 0;
+    
+    for(int i = 0; i < secuencia.size(); i++){
+        for(int j = 0; j < x.size(); j++){
+            if(x[j] > secuencia[i].first && x[j] <= secuencia[i+1].first){
+                double y_pendiente = secuencia[i+1].second - secuencia[i].second;
+                double x_pendiente = secuencia[i+1].first - secuencia[i].first;
+                if(x_pendiente == 0){
+                    return;
+                }
+                double m = y_pendiente / x_pendiente;
+                double b = secuencia[i].second - (m * secuencia[i].first);
+                error += abs(y[j] - (m * x[j] + b));
+            }
+        }
+    }
+    return error;
+}
+//Lista NO TESTEADA
+bool factibilidad(std::vector<std::pair<double,double>> secuencia){
+    for(int i = 0; i < secuencia.size(); i++){
+        if(secuencia[i].first >= secuencia[i+1].first){
+            return false;
+        }
+    }
+    return true;
+}
 
 int main(int argc, char** argv) {
     std::string instance_name = "../../data/titanium.json";
@@ -20,6 +61,8 @@ int main(int argc, char** argv) {
     int n = 6;
     int N = 5;
 
+    std::vector<double> grid_y = instance["y"];
+    std::vector<double> grid_x = instance["x"];
     std::cout << K << std::endl;
 
     // Aca empieza la magia.
