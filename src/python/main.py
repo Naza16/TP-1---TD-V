bf39cf7a7d88eb563249e752d9404ebcbddece55
LucasBrea <35787715+LucasBrea@users.noreply.github.com>
2024-04-22 15:40:54 -0400

    Update main.py

--- src/python/main.py
+++ src/python/main.py
@@ -1,8 +1,9 @@
 import json
+import time
 import numpy as np
-import itertools
 import matplotlib.pyplot as plt
-BIG_NUMBER = 1e10 # Revisar si es necesario.
+start = time.time()
+BIG_NUMBER = 1e10
 
 def lista_breakpoints(m, n,x,y):
     """
@@ -41,49 +42,56 @@ def factibilidad(secuencia):
 def fuerza_bruta(m, n, k, best, x, y, sol_parcial=None):
     if sol_parcial is None:
         sol_parcial = []
-    breakpoints = lista_breakpoints(m, n,x,y)
+    breakpoints = lista_breakpoints(m, n, x, y)
+    
     if len(sol_parcial) == k:
         if factibilidad(sol_parcial):
             current_error = optimalidad(sol_parcial, x, y)
             if current_error < best['obj']:
                 best['obj'] = current_error
                 best['sol'] = sol_parcial.copy()
-    else:
-        for punto in breakpoints:
-                sol_parcial.append(punto)
-                fuerza_bruta(m, n, k, best, x, y, sol_parcial)
-                sol_parcial.remove(punto) 
+        return
+
+    for punto in breakpoints:
+        if punto not in sol_parcial: 
+            sol_parcial.append(punto)
+            fuerza_bruta(m, n, k, best, x, y, sol_parcial)
+            sol_parcial.remove(punto)
 
 def backtracking(m, n, k, best, x, y, sol_parcial=None):
     if sol_parcial is None:
         sol_parcial = []
     breakpoints = lista_breakpoints(m, n,x,y)
+    
     if len(sol_parcial) == k:
         current_error = optimalidad(sol_parcial, x, y)
-        if current_error < best['obj'] and sol_parcial[k-1][0] >= x[len(x)-1]:
+        if current_error < best['obj'] and sol_parcial[k-1][0]>=x[len(x)-1]:
             best['obj'] = current_error
             best['sol'] = sol_parcial.copy()
     else:
         for punto in breakpoints:
                 sol_parcial.append(punto)
-                if factibilidad(sol_parcial) and optimalidad(sol_parcial,x,y)<best['obj'] and sol_parcial[0][0] <= x[0]:
+                if factibilidad(sol_parcial) and optimalidad(sol_parcial,x,y)<best['obj'] and sol_parcial[0][0]<=x[0]:
                     backtracking(m, n, k, best, x, y, sol_parcial)
+
                 sol_parcial.remove(punto) 
 
-def bottom_up_dinamica(m, n, x, y, k):
+
+def dinamica(m, n, x, y, k):
+
     breakpoints = lista_breakpoints(m, n, x, y)
-    dp = [[BIG_NUMBER for _ in range(len(breakpoints))] for _ in range(k+1)]
+    dp = [[BIG_NUMBER for _ in range(len(breakpoints))] for _ in range(k + 1)]
     for j in range(len(breakpoints)):
         dp[1][j] = optimalidad([breakpoints[0], breakpoints[j]], x, y)
-    
-    for i in range(2, k+1): 
+    for i in range(2, k + 1):
         for j in range(1, len(breakpoints)): 
-            for l in range(j): 
-                if factibilidad([breakpoints[l], breakpoints[j]]): 
+            for l in range(j):
+                if factibilidad([breakpoints[l], breakpoints[j]]):
                     error_segmento = optimalidad([breakpoints[l], breakpoints[j]], x, y)
-                    dp[i][j] = min(dp[i][j], dp[i-1][l] + error_segmento)
-    
+                    dp[i][j] = min(dp[i][j], dp[i - 1][l] + error_segmento)
     return min(dp[k])
+
+
 
 def plottear_solucion(x_data, y_data, breakpoints):
     plt.scatter(x_data, y_data, color='blue', label='Data Points')
@@ -94,6 +102,7 @@ def plottear_solucion(x_data, y_data, breakpoints):
         plt.plot([min(x_data), x_breaks[0]], [y_breaks[0], y_breaks[0]], 'r--', linewidth=1)
     if x_breaks[-1] < max(x_data):
         plt.plot([x_breaks[-1], max(x_data)], [y_breaks[-1], y_breaks[-1]], 'r--', linewidth=1)
+
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('PWL Plotteado Sobre los Datapoints')
@@ -101,58 +110,26 @@ def plottear_solucion(x_data, y_data, breakpoints):
     plt.show()
 
 def main():
-    instance_name = input("Que tipo de instancia desea? Aspen (1), Ethanol Water(2), Optimistic(3), Titanium(4) o Toy(5)? Por favor coloque los numeros que indican la instancia")
-    if instance_name == '1':
-        filename = "./data/" + "aspen_simulaton.json"
-        with open(filename) as f:
-            instance = json.load(f)
-    elif instance_name == '2':
-        filename = "./data/" + "ethanol_water_vle.json"
-        with open(filename) as f:
-            instance = json.load(f)
-    elif instance_name == '3':
-        filename = "./data/" + "optimistic_instance.json"
-        with open(filename) as f:
-            instance = json.load(f)
-    elif instance_name == 4:
-        filename = "./data/" + "titanium.json"
-        with open(filename) as f:
-            instance = json.load(f)
-    else:
-        filename = "./data/" + "toy_instance.json"
-        with open(filename) as f:
-            instance = json.load(f)
-
+    # Ejemplo para leer una instancia con json
+    instance_name = "titanium.json"
+    filename = "../../data/" + instance_name
+    with open(filename) as f:
+        instance = json.load(f)
+    
     K = instance["n"]
-    m = 8
-    n = 8
+    m = 6
+    n = 6
     N = 4
-    # Ejemplo para definir una grilla de m x n.
-    #grid_x = np.linspace(min(instance["x"]), max(instance["x"]), num=m, endpoint=True)
-    #grid_y = np.linspace(min(instance["y"]), max(instance["y"]), num=n, endpoint=True)
     
     best = {'obj': BIG_NUMBER, 'sol': set()}
-    fuerza_bruta(m, n, N, best, instance["x"], instance["y"])
+    backtracking(m,n,6,best,instance['x'],instance['y'])
     print("Error:", best['obj'])
     print("Mejor Solucion:", best['sol'])
-    
     plottear_solucion(instance['x'], instance['y'], best['sol'])
 
-    backtracking(m,n,N,best,instance["x"], instance["y"])
-    print("Error" , best['obj'])
-    print("Mejor Solucion: ", best['sol'])
-
-    plottear_solucion(instance['x'], instance['y'], best['sol'])
-
-    bottom_up_dinamica(m,n,N, best, instance["X"], instance["Y"])
-    print("Error:", best['obj'])
-    print("Mejor Solucion: ", best['sol'])
-
-    plottear_solucion(instance['x'], instance['y'], best['sol'])
-
-    plottear_solucion(instance['x'], instance['y'], best['sol'])
-
-
+    #print(dinamica(m,n,instance['x'],instance['y'],50))
+    end=time.time()
+    print(end-start)
 '''
 	best = {}
 	best['sol'] = [None]*(N+1)
